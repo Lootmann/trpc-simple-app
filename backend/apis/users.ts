3dd6ad1d1922a754a0c8efe9f425ad423183eb2e
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-08 03:39:56 +0900

    backend: trpc createUser

--- backend/apis/users.ts
+++ backend/apis/users.ts
@@ -3,3 +3,12 @@ import { prisma } from "../utils/prisma";
 export const getAllUsers = async () => {
   return prisma.user.findMany();
 };
+
+export const createUser = async (name: string, password: string) => {
+  return prisma.user.create({
+    data: {
+      name: name,
+      password: password,
+    },
+  });
+};
--- backend/routers/users.ts
+++ backend/routers/users.ts
@@ -1,6 +1,7 @@
 import { Context } from "../utils/context";
-import { getAllUsers } from "../apis/users";
+import { createUser, getAllUsers } from "../apis/users";
 import { initTRPC } from "@trpc/server";
+import { z } from "zod";
 
 const t = initTRPC.context<Context>().create();
 const publicProcedure = t.procedure;
@@ -14,6 +15,22 @@ export const userRouter = t.router({
     const users = await getAllUsers();
     return users;
   }),
+
+  createUser: publicProcedure
+    .input(
+      z.object({
+        name: z.string().min(3),
+        password: z.string().min(5),
+      })
+    )
+    .output(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      return createUser(input.name, input.password);
+    }),
 });
 
 export type UserRouter = typeof userRouter;
