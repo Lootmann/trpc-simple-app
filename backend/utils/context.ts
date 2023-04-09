6a1c213cd03da2340768949da53c89ac982cb053
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-09 10:44:23 +0900

    backend: trpc context - add fake header process, get token from headers

--- backend/utils/context.ts
+++ backend/utils/context.ts
@@ -3,7 +3,12 @@ import { inferAsyncReturnType } from "@trpc/server";
 
 export async function createContext({ req, res }: CreateExpressContextOptions) {
   // TODO: user authentications
-  return { req, res };
+  async function getUserFromHeader() {
+    return { id: 1, name: "hoge", token: "hogehoge" };
+  }
+
+  const user = await getUserFromHeader();
+  return { req, res, user };
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>;
