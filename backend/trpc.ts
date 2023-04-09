88c0b46bd7e52e20225b4e3c34071d7788888192
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-04-09 12:22:42 +0900

    backend: move authorize process to trpc.ts middleware

--- backend/trpc.ts
+++ backend/trpc.ts
@@ -1,4 +1,5 @@
 import { Context } from "./utils/context";
+import { getUserByName } from "./apis/users";
 import { initTRPC, TRPCError } from "@trpc/server";
 
 const t = initTRPC.context<Context>().create();
@@ -8,14 +9,22 @@ export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
 export const mainRouter = t.mergeRouters;
 
-const isAuthed = middleware(({ next, ctx }) => {
-  if (!ctx.user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
+const isAuthed = middleware(async ({ next, ctx }) => {
+  const { req } = ctx;
+  const accessToken = req.headers.authorization?.split(" ")[1];
+
+  // TODO: check token is valid
+  if (!accessToken)
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "Bearer is missing" });
+
+  // TODO: check user is valid
+  const user = await getUserByName(accessToken);
+  if (!user)
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "token is invalid" });
 
   return next({
     ctx: {
-      user: ctx.user,
+      user: user,
     },
   });
 });
--- backend/utils/context.ts
+++ backend/utils/context.ts
@@ -1,21 +1,8 @@
 import { CreateExpressContextOptions } from "@trpc/server/adapters/express";
-import { getUserByName } from "../apis/users";
 import { inferAsyncReturnType } from "@trpc/server";
 
 export async function createContext({ req, res }: CreateExpressContextOptions) {
-  async function getUserFromHeader() {
-    if (req.headers.authorization) {
-      const username = req.headers.authorization;
-      const user = getUserByName(username);
-      return user;
-    }
-    return null;
-  }
-
-  // NOTE: when Authorization header is valid, returns username
-  // NOTE: when not, returns null
-  const user = await getUserFromHeader();
-  return { req, res, user };
+  return { req, res };
 }
 
 export type Context = inferAsyncReturnType<typeof createContext>;
